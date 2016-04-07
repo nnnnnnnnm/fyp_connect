f25d74077c212c84a32d6d8f6cbe19cc0868ebef
nnnnnnnnm <dickychchan@gmail.com>
2016-04-07 17:32:56 +0800

    modify
    search_details.php
    
    add type:both

--- search_details.php
+++ search_details.php
@@ -112,15 +112,174 @@ if (isset($_GET["type"]) && isset($_GET["keyword"])) {
 			// echo no users JSON
 			echo json_encode($response);
 		}
+	} else if ($type == 'both'){
+		// get a product from products table
+		$result = mysql_query("SELECT * FROM `food` WHERE id LIKE '%$keyword%' OR name LIKE '%$keyword%' OR type LIKE '%$keyword%' OR price LIKE '%$keyword%' OR image LIKE '%$keyword%' OR Restaurantid LIKE '%$keyword%'");
 	
-	} else {
-		// no product found
-		$response["success"] = 0;
-		$response["message"] = "No result found";
+		if (!empty($result)) {
+			// check for empty result
+			if (mysql_num_rows($result) > 0) {
+				// looping through all results
+				// products node
+				$response["food"] = array();
+				
+				while ($row = mysql_fetch_array($result)) {
+					// temp user array
+					$food = array();
+					$food["id"] = $row["id"];
+					$food["name"] = $row["name"];
+					$food["type"] = $row["type"];
+					$food["price"] = $row["price"];
+					$food["image"] = $row["image"];
+					$food["Restaurantid"] = $row["Restaurantid"];
 
-		// echo no users JSON
+					// push single product into final response array
+					array_push($response["food"], $food);
+				}
+				// success
+				$response["success"] = 1;
+				$response["food_result"] = 1;
+
+				
+				
+				// get a product from products table
+				$result = mysql_query("SELECT * FROM restaurant WHERE id LIKE '%$keyword%' OR name LIKE '%$keyword%' OR address LIKE '%$keyword%' OR type LIKE '%$keyword%' OR telNum LIKE '%$keyword%' OR Userid LIKE '%$keyword%'");
+			
+				if (!empty($result)) {
+					// check for empty result
+					if (mysql_num_rows($result) > 0) {
+						// looping through all results
+						// products node
+						$response["restaurant"] = array();
+						
+						while ($row = mysql_fetch_array($result)) {
+							// temp user array
+							$restaurant = array();
+							$restaurant["id"] = $row["id"];
+							$restaurant["name"] = $row["name"];
+							$restaurant["address"] = $row["address"];
+							$restaurant["type"] = $row["type"];
+							$restaurant["telNum"] = $row["telNum"];
+							$restaurant["Userid"] = $row["Userid"];
+
+							// push single product into final response array
+							array_push($response["restaurant"], $restaurant);
+						}
+						// success
+						$response["restaurant_result"] = 1;
+
+						// echoing JSON response
+						echo json_encode($response);
+					} else {
+						// no product found
+						$response["restaurant_result"] = 0;
+						echo json_encode($response);
+					}
+				} else if($response["food_result"] == 0 && $response["restaurant_result"] == 0){
+					// no product found
+					$response["success"] = 0;
+					$response["message"] = "No result found";
+
+					// echo no users JSON
+					echo json_encode($response);
+				}
+			}else {
+				$response["food_result"] = 0;
+				// get a product from products table
+				$result = mysql_query("SELECT * FROM restaurant WHERE id LIKE '%$keyword%' OR name LIKE '%$keyword%' OR address LIKE '%$keyword%' OR type LIKE '%$keyword%' OR telNum LIKE '%$keyword%' OR Userid LIKE '%$keyword%'");
+			
+				if (!empty($result)) {
+					// check for empty result
+					if (mysql_num_rows($result) > 0) {
+						// looping through all results
+						// products node
+						$response["restaurant"] = array();
+						
+						while ($row = mysql_fetch_array($result)) {
+							// temp user array
+							$restaurant = array();
+							$restaurant["id"] = $row["id"];
+							$restaurant["name"] = $row["name"];
+							$restaurant["address"] = $row["address"];
+							$restaurant["type"] = $row["type"];
+							$restaurant["telNum"] = $row["telNum"];
+							$restaurant["Userid"] = $row["Userid"];
+
+							// push single product into final response array
+							array_push($response["restaurant"], $restaurant);
+						}
+						// success
+						$response["restaurant_result"] = 1;
+
+						// echoing JSON response
+						echo json_encode($response);
+					} else {
+						// no product found
+						$response["restaurant_result"] = 0;
+						echo json_encode($response);
+					}
+				} else if($response["food_result"] == 0 && $response["restaurant_result"] == 0){
+					// no product found
+					$response["success"] = 0;
+					$response["message"] = "No result found";
+
+					// echo no users JSON
+					echo json_encode($response);
+				}
+			}	
+		}else {
+			$response["food_result"] = 0;
+			// get a product from products table
+				$result = mysql_query("SELECT * FROM restaurant WHERE id LIKE '%$keyword%' OR name LIKE '%$keyword%' OR address LIKE '%$keyword%' OR type LIKE '%$keyword%' OR telNum LIKE '%$keyword%' OR Userid LIKE '%$keyword%'");
+			
+				if (!empty($result)) {
+					// check for empty result
+					if (mysql_num_rows($result) > 0) {
+						// looping through all results
+						// products node
+						$response["restaurant"] = array();
+						
+						while ($row = mysql_fetch_array($result)) {
+							// temp user array
+							$restaurant = array();
+							$restaurant["id"] = $row["id"];
+							$restaurant["name"] = $row["name"];
+							$restaurant["address"] = $row["address"];
+							$restaurant["type"] = $row["type"];
+							$restaurant["telNum"] = $row["telNum"];
+							$restaurant["Userid"] = $row["Userid"];
+
+							// push single product into final response array
+							array_push($response["restaurant"], $restaurant);
+						}
+						// success
+						$response["restaurant_result"] = 1;
+
+						// echoing JSON response
+						echo json_encode($response);
+					} else {
+						// no product found
+						$response["restaurant_result"] = 0;
+						echo json_encode($response);
+					}
+				} else if($response["food_result"] == 0 && $response["restaurant_result"] == 0){
+					// no product found
+					$response["success"] = 0;
+					$response["message"] = "No result found";
+
+					// echo no users JSON
+					echo json_encode($response);
+				}
+		}
+		
+	} else {
+		// required field is missing
+		$response["success"] = 0;
+		$response["message"] = "Required field(s) is missing";
+
+		// echoing JSON response
 		echo json_encode($response);
-	}
+	}	
 } else {
     // required field is missing
     $response["success"] = 0;

b2180dea0c04aca2987b1e5d7a017612082aad42
nnnnnnnnm <dickychchan@gmail.com>
2016-05-10 21:11:02 +0800

    add order_count
    add order_count

---
+++ get_order_count.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * Following code will get single product details
+ * A product is identified by product id (pid)
+ */
+
+// array for JSON response
+$response = array();
+
+
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+
+// connecting to db
+$db = new DB_CONNECT();
+
+// check for post data
+if (isset($_GET["number"])) {
+    $number = $_GET['number'];
+
+    // get a product from products table
+    $result = mysql_query("SELECT COUNT(Foodid) as count FROM `orderline` WHERE Ordernumber=$number");
+
+    if (!empty($result)) {
+        // check for empty result
+        if (mysql_num_rows($result) > 0) {
+
+            $result = mysql_fetch_array($result);
+
+            $response["number"] = $result["count"];
+
+            // success
+            $response["success"] = 1;
+
+
+            // echoing JSON response
+            echo json_encode($response);
+        } else {
+            // no product found
+            $response["success"] = 0;
+            $response["message"] = "No order found";
+
+            // echo no users JSON
+            echo json_encode($response);
+        }
+    } else {
+        // no product found
+        $response["success"] = 0;
+        $response["message"] = "No order found";
+
+        // echo no users JSON
+        echo json_encode($response);
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+
+    // echoing JSON response
+    echo json_encode($response);
+}
+?>
